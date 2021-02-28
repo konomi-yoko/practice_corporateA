83ce4a1e99a9012f217ca71f8bab90038d35e4f1
DESKTOP-Q693PF6\vivi0 <yokoyama@public-function.co.jp>
2021-03-01 02:08:16 +0900

    完成

--- js/app.js
+++ js/app.js
@@ -6,8 +6,48 @@ $(function(){
   if (window.matchMedia('(max-width:640px)').matches) {
     $('#remove').removeClass('header-bg');
 
+    // topscrpll
+$(document).ready(function(){
+  $("#topBtn").hide();
+  $(window).on("scroll", function() {
+    if ($(this).scrollTop() > 150) {
+      $("#topBtn").fadeIn("fast");
+    } else {
+      $("#topBtn").fadeOut("fast");
+    }
+    scrollHeight = $(document).height();
+    scrollPosition = $(window).height() + $(window).scrollTop();
+    footHeight = $("footer").innerHeight();
+    if ( scrollHeight - scrollPosition  <= footHeight ) {
+      $("#topBtn").css({
+        "position":"absolute",
+        "bottom": footHeight + 15
+        });
+    } else {
+      $("#topBtn").css({
+        "position":"fixed",
+        "bottom": "20px"
+      });
+    }
+  });
+  $('#topBtn').click(function () {
+    $('body,html').animate({
+    scrollTop: 0
+    }, 600);
+    return false;
+  });
+});
 
-
+// nav固定
+jQuery(window).on('scroll', function () {
+    if (jQuery('.js-header-nav').height() < jQuery(this).scrollTop()) {
+        jQuery('.js-header-nav').addClass('change-color');
+        $('.company-name-sp-js').addClass('change-text')
+    } else {
+        jQuery('.js-header-nav').removeClass('change-color');
+        $('.company-name-sp-js').removeClass('change-text');
+    }
+});
 
 
 // PC
@@ -82,5 +122,19 @@ $(function(){
     var position = target.offset().top;
     $("html, body").animate({scrollTop:position}, speed, "swing");
     return false;
+  });
+});
+
+// 画像スクロール時アニメーション
+$(function(){
+  $(window).on('load scroll',function (){
+    $('.animation').each(function(){
+      var target = $(this).offset().top;
+      var scroll = $(window).scrollTop();
+      var height = $(window).height();
+      if (scroll > target - height){
+        $(this).addClass('active');
+      }
+    });
   });
 });
